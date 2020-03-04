778a1368b4fde6fee3d1153d262886fbad3f949f
Stan Slupecki <sslup@hermetikos.com>
2020-03-04 10:26:36 -0500

    Adds a graph implementation to the data struct file

--- data_structures.py
+++ data_structures.py
@@ -1,3 +1,5 @@
+import collections
+
 # Binary tree code borrowed from: https://www.tutorialspoint.com/python_data_structure/python_binary_tree.htm
 class BinaryTreeNode:
     def __init__(self, data):
@@ -19,3 +21,24 @@ class BinaryTreeNode:
                     self.right.insert(data)
         else:
             self.data = data
+
+# GRAPHS
+
+# a model of a graph that uses
+# borrowed from https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
+# this storage method takes O(|V|+|E|) space, for a max of O(V^2)
+# adding a vertex is constant time
+# computing queries take more time O(V)
+
+class GraphAdjacencyList:
+
+    def __init__(self):
+        # dict to store graph
+        self.graph = collections.defaultdict(list)
+
+    # add a new edge
+    def addVertex(self, u):
+        self.graph[u] = []
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
