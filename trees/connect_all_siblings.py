7c0edfed8cc58c35d14157cf86c2522b5f6a5bcf
Stan Slupecki <sslup@hermetikos.com>
2020-08-07 15:48:28 -0400

    Reimplements Connect All Siblings code with more efficient code

--- trees/connect_all_siblings.py
+++ trees/connect_all_siblings.py
@@ -15,43 +15,21 @@ class BinaryTreeWithSiblingNode(BinaryTreeNode):
 
 
 def connect_all_siblings(root):
-    # the core of the code is level order traversal
-    queue = deque()
-    # each value in the queue will be a tuple
-    # the first value is the node itself
-    # the second is the level it was found at
-    queue.append((root, 1))
+    # we need to track the current node and the last node we worked on
+    # initially, set the previous node to equal the current node
+    current_node = previous_node = root
 
-    # create a hash table (dictionary) to hold the last found value at a level
-    last_found_node = {}
-
-    # while there are still nodes to process
-    while queue:
-        # get the next node to process, and its level
-        current_node, level = queue.popleft()
-
-        # here we decide if the current node is another node's sibling
-        # if it's the root, it has no siblings
-        # so only try when the level is greater than 1
-        if level > 1:
-            # there are two options from here
-            # first is if this node is the sibling of a node on this level
-            # this means that there will be a value stored for this level in the dict, last_found_node
-            if level in last_found_node:
-                # if this is the case, set the current node as the sibling of the previous node
-                previous_node = last_found_node[level]
-                previous_node.sibling = current_node
-            # otherwise, the previous node will be the last found node of the previous level
-            else:
-                previous_node = last_found_node[level-1]
-                previous_node.sibling = current_node
-        # regardless, set the current node as the last found node at the current level
-        last_found_node[level] = current_node
-
-        # then add any children to the queue
-        # pair with their level
+    while current_node is not None:
+        # note that this code works by "looking ahead" into the next level of the tree
+        # each node in a level is linked to its sibling when its parent is visited
         if current_node.left is not None:
-            queue.append((current_node.left, level+1))
+            previous_node.sibling = current_node.left
+            previous_node = current_node.left
+
         if current_node.right is not None:
-            queue.append((current_node.right, level+1))
+            previous_node.sibling = current_node.right
+            previous_node = current_node.right
+
+        previous_node.sibling = None
+        current_node = current_node.sibling
 
