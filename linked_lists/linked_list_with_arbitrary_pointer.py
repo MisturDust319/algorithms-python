4825fcc57412bb10ad8c0710fa0bdaf20db64aad
MisturDust319 <jarlofearls@gmail.com>
2020-08-25 15:20:03 -0400

    Adds solution to Linked List with Arbitrary Pointers problem

---
+++ linked_lists/linked_list_with_arbitrary_pointer.py
@@ -0,0 +1,66 @@
+# from: https://www.educative.io/m/copy-linked-list-with-arbitrary-pointer
+# "You are given a linked list where the node has two pointers. The first is the regular next pointer.
+# The second pointer is called arbitrary_pointer and it can point to any node in the linked list.
+# Your job is to write code to make a deep copy of the given linked list."
+
+class LinkedListNodeWithArbitraryPointer:
+    """
+    A Linked List node that also supports a pointer to an arbitrary node
+    """
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.arbitrary = None
+
+def deep_copy_linked_list_with_arbitrary_pointer(head):
+    # if the head node is empty, return None
+    # this means we've been passed an empty list
+    if head is None:
+        return None
+
+    # start the process at the head node
+    # an init a new head node to be returned by the function
+    current_node =  head
+    # this variable will point to the head node of the linked list copy
+    new_head = None
+    # this variable holds the last node added to the new linked list
+    previous_new_node = None
+
+    # this dictionary will be used to track the unique nodes we discover
+    unique_nodes = {}
+
+    # in the first pass, you link the new linked list's arbitrary pointers
+    # to the arbitrary pointers in the original list
+    # creating copies of the arbitrary pointers is
+    while current_node is not None:
+        # create a new node with a copy of the current node's data
+        new_node = LinkedListNodeWithArbitraryPointer(current_node.data)
+
+        # create a pointer to the old node's arbitrary value
+        # we'll copy it on another pass
+        new_node.arbitrary = current_node.arbitrary
+
+        # we need to check if we've added any values to the new list
+        # if the previous new node has a value
+        # that means we've started the new list copy...
+        if previous_new_node is not None:
+            #...and in response, you should
+            previous_new_node.next = new_node
+        else:
+            new_head = new_node
+
+        unique_nodes[current_node] = new_node
+
+        previous_node = new_node
+        current_node = current_node.next
+
+    current_node = new_head
+
+    # next copy the arbitrary pointer
+    while current_node is not None:
+        if current_node.arbitrary is not None:
+            new_node = unique_nodes[current_node.arbitrary]
+
+            current_node.arbitrary = new_node
+
+        current_node = current_node.next
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -4,6 +4,7 @@ import unittest
 import data_structures
 
 import linked_lists.linked_list_sum
+from linked_lists.linked_list_with_arbitrary_pointer import LinkedListNodeWithArbitraryPointer, deep_copy_linked_list_with_arbitrary_pointer
 
 class TestLinkedListSum(unittest.TestCase):
     def test_linked_list_sum(self):
@@ -48,4 +49,77 @@ class TestLinkedListSum(unittest.TestCase):
 
         self.assertListEqual(results, expected_result)
 
+class TestLinkedListWithArbitraryPointer(unittest.TestCase):
+    def setUp(self):
+        # create a linked list like this
+        # 0->1->2->3->4
+        # \____/\____/
+        # set up the next pointers
+        self.linked_list = LinkedListNodeWithArbitraryPointer(0)
+        self.linked_list.next = LinkedListNodeWithArbitraryPointer(1)
+        self.linked_list.next.next = LinkedListNodeWithArbitraryPointer(2)
+        self.linked_list.next.next.next = LinkedListNodeWithArbitraryPointer(3)
+        self.linked_list.next.next.next.next = LinkedListNodeWithArbitraryPointer(4)
+        # set up the arbitrary pointers
+        self.linked_list.arbitrary = self.linked_list.next.next
+        self.linked_list.next.next = self.linked_list.next.next.next.next
 
+    def compareLists(self, head1, head2):
+        """
+        compare two linked lists with arbitrary pointers
+        :param head1:
+        head node of first list
+        :param head2:
+        head node of second list
+        :return:
+        True if the two lists have the same data
+        False otherwise
+        """
+        # create two index nodes
+        # point them to the head nodes
+        current_node_1 = head1
+        current_node_2 = head2
+
+        while (current_node_1 is not None
+            and current_node_2 is not None):
+            # check if the two nodes share the same data
+            # if not, return false
+            if current_node_1.data != current_node_2.data:
+                return False
+
+            # check if both have an arbitrary node
+            # if one has one and the other doesn't, return false
+            if current_node_1.arbitrary is None and current_node_2.arbitrary is not None:
+                return False
+            if current_node_2.arbitrary is None and current_node_1.arbitrary is not None:
+                return False
+            # then check if the arbitrary nodes point to the same data
+            if (current_node_1.arbitrary is not None
+                and current_node_2.arbitrary is not None):
+                if current_node_1.arbitrary.data != current_node_2.arbitrary.data:
+                    return False
+            
+            # advance the index nodes
+            current_node_1 = current_node_1.next
+            current_node_2 = current_node_2.next
+
+            # if one node has reached it's end, and the other hasn't,
+            # return false
+            if current_node_1 is None and current_node_2 is not None:
+                return False
+            elif current_node_2 is None and current_node_1 is not None:
+                return False
+
+        # if no discrepancy has been found, return true
+        return True
+
+
+
+    def test_linked_list_with_arbitrary_pointer(self):
+        # attempt to copy the list
+        list_copy = deep_copy_linked_list_with_arbitrary_pointer(self.linked_list)
+
+        # compare the lists
+        result = self.compareLists(self.linked_list, list_copy)
+
+        self.assertTrue(result)
