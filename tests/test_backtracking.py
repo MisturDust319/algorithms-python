0786ebcbedc496df88fd0d17ef6ebf435a52609e
MisturDust319 <jarlofearls@gmail.com>
2020-10-16 22:56:44 -0400

    Adds tests for boggle problem, and info for backtracking tests

---
+++ tests/test_backtracking.py
@@ -0,0 +1,22 @@
+"""
+Tests for backtracking problems
+"""
+import pytest
+
+from backtracking.boggle import find_all_strings
+
+
+@pytest.mark.backtracking
+@pytest.mark.strings
+def test_find_all_strings():
+    # test data borrowed from: https://www.educative.io/m/boggle
+    grid = [['c', 'a', 't'],
+            ['r', 'r', 'e'],
+            ['t', 'o', 'n']]
+
+    dictionary = {"cat", "cater", "cartoon", "toon", "moon", "not", "tone", "apple", "ton", "art"}
+
+    result = find_all_strings(grid, dictionary)
+    expected_result = ["not", "cat", "art", "cater", "ton", "eat"]
+
+    assert all([ result == expected for result, expected in zip(result, expected_result)]) is True
